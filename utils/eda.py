54f660a18ae60445c9f85b3d8998fe4b2f942108
Biola <52200708+decorouz@users.noreply.github.com>
2024-05-29 09:41:06 +0100

    update eda

--- utils/eda.py
+++ utils/eda.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+
 ## Bivariate Analysis FIES with socio-demographic, shock and difficulty
 def fies_by_socio_demo(df, col):
     """
@@ -23,7 +24,7 @@ def fies_by_socio_demo(df, col):
         id_vars=[col, "weight_final"],
         value_vars=["prob_mod_sev", "prob_sev"],
         var_name="RFI_level",
-        value_name="prob_value"
+        value_name="prob_value",
     )
 
     # Calculate weighted percentage for each state and RFI
@@ -32,15 +33,17 @@ def fies_by_socio_demo(df, col):
         .agg(
             percentage=(
                 "prob_value",
-                lambda x: (x * melted_df.loc[x.index, "weight_final"]*100).sum()
+                lambda x: (x * melted_df.loc[x.index, "weight_final"] * 100).sum()
                 / melted_df.loc[x.index, "weight_final"].sum(),
             )
-        ).reset_index()
+        )
+        .reset_index()
     )
     return weighted_percent
 
+
 # Plot the RFI levels by different variables such as state, gender etc
-def plot_fies_levels_by_vars(grp_df, var="state",kind="bar", figsize=(7,4), **kwargs):
+def plot_fies_levels_by_vars(grp_df, var="state", kind="bar", figsize=(7, 4), **kwargs):
     """
     Plot barplot showing the levels of RFI by a specified variable.
     e.g state, gender
@@ -56,7 +59,7 @@ def plot_fies_levels_by_vars(grp_df, var="state",kind="bar", figsize=(7,4), **kw
     """
     # Sort the bar in descending order
     grp_df.sort_values("prob_mod_sev", inplace=True, ascending=False)
-    
+
     ax = grp_df.plot(kind=kind, figsize=figsize)
     ax.set_xlabel("")
     plt.ylabel("Percentage (%)")
@@ -64,16 +67,20 @@ def plot_fies_levels_by_vars(grp_df, var="state",kind="bar", figsize=(7,4), **kw
     ax.set_title(kwargs["title"])
     plt.legend(title="RFI Level", bbox_to_anchor=(1.05, 1), loc="upper left")
     plt.xticks(rotation=0)
-    
+
     for p in ax.patches:
         if kind == "barh":
-             # Remove x-axis ticks
+            # Remove x-axis ticks
             # p.xaxis.set_ticks([])
             width, height = p.get_width(), p.get_height()
             y, x = p.get_xy()
             ax.annotate(
-                f"{width:.1f}%", (width, x + height / 2),
-                ha="left", va="center", fontsize=8)
+                f"{width:.1f}%",
+                (width, x + height / 2),
+                ha="left",
+                va="center",
+                fontsize=8,
+            )
         else:
             width = p.get_width()
             height = p.get_height()
@@ -87,9 +94,9 @@ def plot_fies_levels_by_vars(grp_df, var="state",kind="bar", figsize=(7,4), **kw
                 xytext=(0, 5),
                 textcoords="offset points",
             )
-    
+
     # plt.tight_layout()
-    
+
     plt.show()
 
 
@@ -125,7 +132,9 @@ def data_type_converter(
     return df
 
 
-def compute_groupby(df: pd.DataFrame, col1: str, col2: str, weight_col: str="weight_final") -> pd.DataFrame:
+def compute_groupby(
+    df: pd.DataFrame, col1: str, col2: str, weight_col: str = "weight_final"
+) -> pd.DataFrame:
     """
     Calculate the percentage of occurrences for two categorical variables in a DataFrame.
 
@@ -156,50 +165,53 @@ def compute_groupby(df: pd.DataFrame, col1: str, col2: str, weight_col: str="wei
     return pivot_df_percentage
 
 
-
-def plot_grouped_data(pivot_df_percentage: pd.DataFrame, 
-                      kind="barh", 
-                      figsize=(12, 6),
-                      **kwargs) -> None:
+def plot_grouped_data(
+    pivot_df_percentage: pd.DataFrame, kind="barh", figsize=(12, 6), **kwargs
+) -> None:
     # Plot the stacked bar chart
     ax = pivot_df_percentage.plot(
-        kind= kind,
+        kind=kind,
         # stacked=kwargs["stacked"],
-        width=0.8, figsize=figsize,
+        width=0.8,
+        figsize=figsize,
         # hatch=['^']*len(pivot_df_percentage)
     )
 
     plt.title(kwargs["title"])
     plt.legend(title=kwargs["legend_title"], bbox_to_anchor=(1.05, 1), loc="upper left")
 
-   
     # Remove y_label
     ax.set_ylabel("")
 
-        
     # Annotate each bar with the percentages for each category
     for bar in ax.patches:
         if kind == "barh":
-             # Remove x-axis ticks
+            # Remove x-axis ticks
             ax.xaxis.set_ticks([])
             width, height = bar.get_width(), bar.get_height()
             y, x = bar.get_xy()
             ax.annotate(
-                f"{width:.1f}%", (width, x + height / 2),
-                ha="left", va="center", fontsize=8)
+                f"{width:.1f}%",
+                (width, x + height / 2),
+                ha="left",
+                va="center",
+                fontsize=8,
+            )
         else:
             ax.yaxis.set_ticks([])
-            ax.annotate(f"{bar.get_height():.1f}%", 
-               (bar.get_x() + bar.get_width() / 2, 
-                bar.get_height()), ha='center', va='center',
-               size=7, xytext=(0, 5),
-               textcoords='offset points')
-                
-            
-    
+            ax.annotate(
+                f"{bar.get_height():.1f}%",
+                (bar.get_x() + bar.get_width() / 2, bar.get_height()),
+                ha="center",
+                va="center",
+                size=7,
+                xytext=(0, 5),
+                textcoords="offset points",
+            )
 
     plt.tight_layout()
     plt.show()
+
 
 def calculate_weighted_percentage(
     df: pd.DataFrame, group_col: str, weight_col: str
@@ -267,9 +279,11 @@ def plot_group_by_percentage(grp_df: pd.DataFrame, grp_by_col: str, **kwargs) ->
     # Show the plot
     plt.show()
 
-#=================================================
+
+# =================================================
 # Crop production difficulty utility functions
-#=================================================
+# =================================================
+
 
 # Main reported crop production difficulty
 # ?>>?Come back to this
@@ -288,40 +302,46 @@ def percentage_crp_prod_dif(df, start, end, n):
         if col != "weight_final":
             # Drop rows where the variable is NaN
             valid_data = df.dropna(subset=[col])
-            
+
             # Calculate weighted sum for each category
             weighted_sum = valid_data.groupby(col, observed=True)["weight_final"].sum()
-            
+
             # Calculate total weight
             total_weight = valid_data["weight_final"].sum()
-            
+
             # Calculate percentage for each category
             percentage = (weighted_sum / total_weight) * 100
             # Add the result to the results DataFrame
             results = pd.concat([results, percentage.rename(col)], axis=1)
     # Melt the result
-    results = pd.melt(results.reset_index(drop=True).iloc[[1]], var_name="difficulty_type", 
-        value_name="value").sort_values("value",ascending=False)
+    results = pd.melt(
+        results.reset_index(drop=True).iloc[[1]],
+        var_name="difficulty_type",
+        value_name="value",
+    ).sort_values("value", ascending=False)
     return results.reset_index(drop=True).head(n)
 
 
-
-
 # =====================Plot all production difficulty variables===========================
-def plot_diff_variables(crp_proddiff_df: pd.DataFrame, title:str) -> None:
+def plot_diff_variables(crp_proddiff_df: pd.DataFrame, title: str) -> None:
     # Plot the shocks
     plt.figure(figsize=(10, 4))
-    ax = sns.barplot(crp_proddiff_df, x="value", y="difficulty_type",)
-    
+    ax = sns.barplot(
+        crp_proddiff_df,
+        x="value",
+        y="difficulty_type",
+    )
+
     # Add percentage labels to the bars
-    ax.bar_label(ax.containers[0], fontsize=8, fmt="%1.1f%%") 
-    
-    # Remove x-axis ticks and lables 
+    ax.bar_label(ax.containers[0], fontsize=8, fmt="%1.1f%%")
+
+    # Remove x-axis ticks and lables
     ax.xaxis.set_ticks([])
     ax.set_xlabel("")
     ax.set_title(title)
-    
-    plt.show();
+
+    plt.show()
+
 
 # ==================== Crop Production Difficult by State ==================================
 # Utitity function to compute all crop difficulty by state
