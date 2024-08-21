7f6073b91829ea2039d78fb89c12cbcc080735d3
Biola <52200708+decorouz@users.noreply.github.com>
2024-08-21 02:05:57 +0100

    add r notebook

--- FIES/MISC_NG_6/notebooks/hh_mics6_modelling.R
+++ FIES/MISC_NG_6/notebooks/hh_mics6_modelling.R
@@ -43,7 +43,24 @@ logLik(fita)-logLik(model)
 # The test statistic is 1697.5874 (-2*(-848.7937)) with 1 degree of freedom, so there is
 # strong evidence that the between-community variance is non-zero.
 
+u0 <- ranef(model, postVar = TRUE)
+u0se <- sqrt(attr(u0[[1]], "postVar")[1, , ])
+commid <- as.numeric(rownames(u0[[1]]))
+u0tab <- cbind("commid" = commid, "u0" = u0[[1]], "u0se" = u0se)
+colnames(u0tab)[2] <- "u0"
+u0tab <- u0tab[order(u0tab$u0), ]
+u0tab <- cbind(u0tab, c(1:dim(u0tab)[1]))
+u0tab <- u0tab[order(u0tab$commid), ]
+colnames(u0tab)[4] <- "u0rank" 
 
+plot(u0tab$u0rank, u0tab$u0, type = "n", xlab = "u_rank", ylab = "conditional
+modes of r.e. for comm_id:_cons", ylim = c(-4, 4)) 
+
+segments(u0tab$u0rank, u0tab$u0 - 1.96*u0tab$u0se, u0tab$u0rank, u0tab$u0 +
+           1.96*u0tab$u0se)
+points(u0tab$u0rank, u0tab$u0, col = "blue")
+abline(h = 0, col = "red")
+str(data)
 ### Model I: Model containing only individual/household-level predictors
 
 model1 <- glmer(formula = MSI ~ hhsex + hh_age_cat + hh_siz_cat  + 
